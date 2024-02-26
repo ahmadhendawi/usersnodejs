622e7d3bb92e791e7bf508bbd76f12bb61f7e870
ahmadhendawi <ahmad.hendawi200255@gmail.com>
2024-02-26 10:00:57 +0300

    z

--- node.js
+++ node.js
@@ -3,8 +3,7 @@ var app=express();
 
 const {MongoClient}=require('mongodb')
 
-var connection="mongodb+srv://ahmadhendawi:0sp6RThO5RfttqXZ@cluster0.uswjlnp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
+var connection="mongodb+srv://ahmadhendawi:HI99JACkKdNRCNOC@cluster0.uswjlnp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster"
 const client= new MongoClient(connection)
 
 const mydb= client.db('test')
@@ -18,13 +17,11 @@ app.get("/", function(req,res)
 })
 
 app.get("/users",async(req,res)=>{
-     //find  =>{}=>all
      const users= await collection.find({}).toArray() 
      res.send(users)
 })
 
 app.get("/user/:username",async(req,res)=>{
-     //find  =>{}=>all
      const users= await collection.findOne({'username':req.params.username}) 
      res.send(users)
 })
@@ -35,15 +32,24 @@ var urlEncoded= bodyParse.urlencoded({extended:false})
 
 app.get("/form", function(req,res)
 {
-   // retunrn form .html 
+
 })
 
+var fs= require('fs')
+
+app.get("/userinfo", function(req,res){
+
+
+    var current= localStorage.getItem('currentUser')
+    res.json(data)
+})
 
 app.post("/login",urlEncoded, async(req,res)=>
 {
       const finduser= await collection.findOne({'userName':req.body.userName})
       if (finduser) 
-      {
+      {  
+          localStorage.setItem('currentUser', finduser)
           res.sendFile(__dirname+"/userInfo.html")
       }
       else{
@@ -53,7 +59,7 @@ app.post("/login",urlEncoded, async(req,res)=>
 
 
 app.post("/register",urlEncoded, async(req,res)=>
-{    //find 
+{  
      const createuser= await collection.insertOne({'userName': req.body.userName})
     
 })
@@ -62,10 +68,10 @@ app.post("/register",urlEncoded, async(req,res)=>
 
 
 
-var server= app.listen(9000,function()
+var server= app.listen(8000,function()
 {
      var host = server.address().address
      var port=server.address().port
 
-     console.log("start my one")
+     console.log("start")
 })
